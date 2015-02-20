e2cb8fc8f69e970cfba2f9cf6630d3890bd0d259
gshrikant <shrikantgiridhar@gmail.com>
2015-02-21 00:11:11 +0530

    backup script

---
+++ python/backup.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# backup.py
+# Backs up selected folders through rsnapshot/rsync over a network
+
+from os import all
+from sys import *
+
+def get_dir():
+    """
+    Device/directory/partition to back up to.
+    """
+def write_log():
+    """ Write log to file and exit """
+
+def main():
+
+
+
+if __name__ == '__main__':
+    main()
