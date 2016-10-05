9b96e8ae911367b99379e9548f677a438a58962d
gshrikant <gshrik@gmail.com>
2016-10-06 01:48:48 +0530

    Caps lock key notifier

---
+++ bash/caps.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+caps_stat="off"
+old_stat=$caps_stat
+
+while true;
+do
+    while read num caps lock stat x;
+    do
+        caps_stat=$stat     # update
+        if [ $caps_stat != $old_stat ];
+        then
+            old_stat=$caps_stat
+            notify-send "Caps Lock" "${caps_stat^^}" -i input-keyboard -t 25
+        fi
+    done <<< "$(xset -q | grep "Caps Lock")"
+done
+
+
